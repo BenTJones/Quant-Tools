ad0451b2fda535202b2fb0485e18b758c1f1bf7c
Ben Jones <benjones012105@gmail.com>
2025-07-26 14:04:05 +0100

    Some name changes and minor tweaks

--- quant_tools/data_loader.py
+++ quant_tools/data_loader.py
@@ -21,20 +21,15 @@ def get_data(tickers, start='2015-01-01', end=None):
         
     # Check if the cached file exists
     if os.path.exists(path):
-        print("Loading data from cache...")
         df = pd.read_parquet(path)
     else:
-        print("Downloading data...")
-        # Download all tickers at once
         df_raw = yf.download(tickers, start=start, end=end, auto_adjust=False, progress=False)
         
         # Reshape the data by stacking the 'Ticker' level from columns to rows
         df = df_raw.stack()
         
-        # Save the clean, reshaped data to the cache
         df.to_parquet(path)
         
-    # Ensure the index names are correct
     df.index.names = ['Date', 'Ticker']
     
     return df
